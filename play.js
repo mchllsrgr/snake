2c64772d0db132954f1a7efed554392b4a7577cb
Michelle Siregar <michellesiregar@gmail.com>
2019-10-23 20:13:33 +0000

    refactored play.js by exporting function to client.js

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.88.149',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  
-  console.log('You\'re dead')
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
