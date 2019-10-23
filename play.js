a681da77ce5ea110734edeac79359c8636b873f8
Michelle Siregar <michellesiregar@gmail.com>
2019-10-23 21:00:38 +0000

    added stdin to take user input, set to handle ctrl + c to quit

--- play.js
+++ play.js
@@ -1,4 +1,20 @@
 const { connect } = require('./client');
 
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => handleUserInput(key));
+  return stdin;
+}
+
 console.log('Connecting ...');
 connect();
+setupInput(handleUserInput);
