8313f14d38b25834587e9fe2c08f8fd57da24d66
Michelle Siregar <michellesiregar@gmail.com>
2019-10-23 22:26:52 +0000

    established constant variables

--- client.js
+++ client.js
@@ -1,19 +1,19 @@
 const net = require('net');
+const { IP, PORT, name } = require('./constants');
 
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '192.168.88.149',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-    conn.write('Name: MCL');
-    // conn.write('Move: up');
+    conn.write(`Name: ${name}`);
   });
 
   conn.on('data', (data) => {
---
+++ constants.js
@@ -0,0 +1,9 @@
+const IP = '192.168.88.149';
+const PORT = 50541;
+const name = 'MCL';
+
+module.exports = {
+  IP,
+  PORT,
+  name
+}
